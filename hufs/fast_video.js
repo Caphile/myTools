fb9a65f9a92cad9d33c86b0c81c59e4abd87f29b
Heechan Kim <caphile98@gmail.com>
2024-12-12 01:41:10 +0900

    의무교육 시간단축용

---
+++ hufs/fast_video.js
@@ -0,0 +1,1 @@
+document.querySelector('#video-div > iframe').src = 'https://howtovideos.hosted.panopto.com/Panopto/Pages/Embed.aspx?id=8c2b2fcd-fdd0-4b1d-b1a3-ac63017510c2&remoteEmbed=true&remoteHost=https%3A%2F%2Fonline-lecture.hufs.ac.kr&embedApiId=video-div&interactivity=none&showtitle=false&showbrand=false&autoplay=true&hideoverlay=false';
