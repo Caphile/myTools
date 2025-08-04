900a6651c1af12cea3dbb6596e9f9e5e4eb7238c
Chan <caphile98@gmail.com>
2025-08-04 15:55:14 +0900

    시스템 GUI화

---
+++ crawling/swim/ClassScraper.py
@@ -0,0 +1,58 @@
+import requests
+from bs4 import BeautifulSoup
+
+def check_remaining_classes():
+    # 사당문화센터 : 3, 삼일수영장 : 7
+    center_num = 3
+    center = 'DONGJAK0' + str(center_num)
+
+    base_url = 'https://sports.idongjak.or.kr/home/171'
+    home_url = base_url + f'?center={center}&category1=28&category2=ALL&title=&train_day='
+    # 카테고리 문제 해결 필요
+
+    response = requests.get(home_url)
+    response.raise_for_status()
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    table = soup.find('table', class_='list_lecture all_border')
+    rows = table.find('tbody', class_='txtcenter').find_all('tr')
+
+    indices_to_remove = {0, 5, 6, 7}
+    seen_ids = set()
+
+    remain_class_data = []
+    for row in rows:
+        cols = row.find_all(['td', 'th'])
+        cols_text = [col.get_text(strip=True) for col in cols]
+
+        class_id = row.get('data-classcd')
+        if class_id in seen_ids:
+            continue
+        seen_ids.add(class_id)
+
+        link_tag = row.find('a', href=True)
+        if link_tag:
+            link = link_tag['href']
+            linked_url = base_url + link
+            
+            detail_response = requests.get(linked_url)
+            detail_response.raise_for_status()
+            
+            detail_soup = BeautifulSoup(detail_response.text, 'html.parser')
+
+            try:
+                info_data = detail_soup.select_one('#contents > article > div > div > div.infomation > div.info_data > dl > dd:nth-child(12)')
+                remain_cnt = info_data.get_text(separator=' ').strip()
+            except:
+                info_data = detail_soup.select_one('#form_lecture_reg > fieldset > div > div.proc_read > div.infomation > div.info_data > dl > dd:nth-child(12)')
+                remain_cnt = info_data.get_text(separator=' ').strip()
+
+            cols_text.append(f'잔여{remain_cnt}')
+            cols_text.append(linked_url)
+            filtered_text = [item for idx, item in enumerate(cols_text) if idx not in indices_to_remove]
+
+        if remain_cnt != '마감':
+            remain_class_data.append(filtered_text)
+
+    return remain_class_data
---
+++ crawling/swim/Modal.py
@@ -0,0 +1,98 @@
+import tkinter as tk
+import sys
+import threading
+import webbrowser
+
+import ClassScraper
+
+#----------------------------------------------------------------------------
+# Tkinter 창 설정
+root = tk.Tk()
+root.title("잔여 강좌 확인창")
+root.geometry("1000x400")
+root.resizable(False, False)
+
+# 상단프레임
+top_frame = tk.Frame(root)
+top_frame.pack(expand=True, fill='both')
+
+# 하단프레임
+bottom_frame = tk.Frame(root, height=50)
+bottom_frame.pack(fill='x', side='bottom')
+
+# 상태 라벨(하단프레임 좌측 배치)
+status_label = tk.Label(bottom_frame, text="상태:", width=10, anchor='w')
+status_label.pack(side='left', padx=10)
+
+# 체크박스(하단프레임 우측 배치)
+check_var = tk.BooleanVar()
+def toggle_topmost():
+    if check_var.get():
+        root.attributes("-topmost", True)
+    else:
+        root.attributes("-topmost", False)
+check_box = tk.Checkbutton(bottom_frame, text="화면고정", variable=check_var, command=toggle_topmost)
+check_box.pack(side='right', padx=10)
+
+# 텍스트 위젯(상단프레임 배치)
+text_widget = tk.Text(top_frame, wrap='word', font=("Consolas", 10))
+text_widget.pack(expand=True, fill='both')
+text_widget.config(state='disabled')
+
+# stdout 리디렉션 클래스
+class RedirectText:
+    def __init__(self, widget):
+        self.widget = widget
+
+    def write(self, string):
+        self.widget.config(state='normal')
+        self.widget.insert('end', string)
+        self.widget.config(state='disabled')
+
+    def flush(self):
+        pass
+
+sys.stdout = RedirectText(text_widget)
+sys.stderr = RedirectText(text_widget)
+#----------------------------------------------------------------------------
+
+def open_link(event):
+    # 클릭한 위치에서 해당 라인의 시작과 끝 인덱스를 얻어 URL을 추출
+    start_index = text_widget.index(tk.CURRENT)
+    
+    # 클릭된 텍스트가 포함된 라인의 시작과 끝 인덱스를 찾기
+    line_start = f"{start_index.split('.')[0]}.0"  # 해당 라인의 시작
+    line_end = f"{start_index.split('.')[0]}.end"  # 해당 라인의 끝
+    line_text = text_widget.get(line_start, line_end)
+
+    webbrowser.open(line_text)
+
+# 실행 루프
+def run_loop():
+    # check_remain_class 함수 호출
+    classes = ClassScraper.check_remaining_classes()
+
+    # 텍스트 출력
+    for cnt, class_row in enumerate(classes):
+        last_text = class_row[-1]   # 링크 주소
+
+        text_widget.config(state='normal')  # 편집 가능하도록 설정
+        text_widget.insert('end', str(cnt + 1) + '.' + '\n')
+        text_widget.insert('end', ' / '.join(class_row[:3]) + '\n')
+        text_widget.insert('end', ' / '.join(class_row[3:-1]) + '\n')
+        text_widget.insert('end', last_text + '\n')
+
+        # 하이퍼링크 스타일을 적용
+        start_index = text_widget.index("end-2l")
+        end_index = text_widget.index("end-1c")
+
+        text_widget.tag_add("hyperlink", start_index, end_index)
+        text_widget.tag_configure("hyperlink", foreground="blue", underline=True)
+
+        # 클릭 이벤트 바인딩 (Label 위젯에서 텍스트 클릭 시 링크 열기)
+        text_widget.tag_bind("hyperlink", "<Button-1>", open_link)
+        text_widget.config(state='disabled')  # 다시 disabled 상태로 설정
+
+# 스레드로 실행
+threading.Thread(target=run_loop, daemon=True).start()
+root.mainloop()
